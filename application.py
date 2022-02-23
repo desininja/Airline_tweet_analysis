9b94c2053bb97133c676cb96eb866144b1b52dc8
Himanshu Bhatt <66287451+desininja@users.noreply.github.com>
2022-02-23 10:09:32 +0530

    Update application.py

--- application.py
+++ application.py
@@ -34,39 +34,39 @@ st.sidebar.markdown("### Number of tweets by sentiment")
 select = st.sidebar.selectbox('Visualization type', ['Bar plot', 'Pie chart'], key='1')
 sentiment_count = data['airline_sentiment'].value_counts()
 sentiment_count = pd.DataFrame({'Sentiment':sentiment_count.index, 'Tweets':sentiment_count.values})
-if not st.sidebar.checkbox("Hide", True):
-    st.markdown("### Number of tweets by sentiment")
-    if select == 'Bar plot':
-        fig = px.bar(sentiment_count, x='Sentiment', y='Tweets', color='Tweets', height=500)
-        st.plotly_chart(fig)
-    else:
-        fig = px.pie(sentiment_count, values='Tweets', names='Sentiment')
-        st.plotly_chart(fig)
+#if not st.sidebar.checkbox("Hide", True):
+st.markdown("### Number of tweets by sentiment")
+if select == 'Bar plot':
+    fig = px.bar(sentiment_count, x='Sentiment', y='Tweets', color='Tweets', height=500)
+    st.plotly_chart(fig)
+else:
+    fig = px.pie(sentiment_count, values='Tweets', names='Sentiment')
+    st.plotly_chart(fig)
 
 st.sidebar.subheader("When and where are users tweeting from?")
 hour = st.sidebar.slider("Hour to look at", 0, 23)
 modified_data = data[data['tweet_created'].dt.hour == hour]
-if not st.sidebar.checkbox("Close", True, key='1'):
-    st.markdown("### Tweet locations based on time of day")
-    st.markdown("%i tweets between %i:00 and %i:00" % (len(modified_data), hour, (hour + 1) % 24))
-    st.map(modified_data)
-    if st.sidebar.checkbox("Show raw data", False):
-        st.write(modified_data)
+#if not st.sidebar.checkbox("Close", True, key='1'):
+st.markdown("### Tweet locations based on time of day")
+st.markdown("%i tweets between %i:00 and %i:00" % (len(modified_data), hour, (hour + 1) % 24))
+st.map(modified_data)
+    #if st.sidebar.checkbox("Show raw data", False):
+    #    st.write(modified_data)
 
 
 st.sidebar.subheader("Total number of tweets for each airline")
 each_airline = st.sidebar.selectbox('Visualization type', ['Bar plot', 'Pie chart'], key='2')
 airline_sentiment_count = data.groupby('airline')['airline_sentiment'].count().sort_values(ascending=False)
 airline_sentiment_count = pd.DataFrame({'Airline':airline_sentiment_count.index, 'Tweets':airline_sentiment_count.values.flatten()})
-if not st.sidebar.checkbox("Close", True, key='2'):
-    if each_airline == 'Bar plot':
-        st.subheader("Total number of tweets for each airline")
-        fig_1 = px.bar(airline_sentiment_count, x='Airline', y='Tweets', color='Tweets', height=500)
-        st.plotly_chart(fig_1)
-    if each_airline == 'Pie chart':
-        st.subheader("Total number of tweets for each airline")
-        fig_2 = px.pie(airline_sentiment_count, values='Tweets', names='Airline')
-        st.plotly_chart(fig_2)
+#if not st.sidebar.checkbox("Close", True, key='2'):
+if each_airline == 'Bar plot':
+    st.subheader("Total number of tweets for each airline")
+    fig_1 = px.bar(airline_sentiment_count, x='Airline', y='Tweets', color='Tweets', height=500)
+    st.plotly_chart(fig_1)
+if each_airline == 'Pie chart':
+    st.subheader("Total number of tweets for each airline")
+    fig_2 = px.pie(airline_sentiment_count, values='Tweets', names='Airline')
+    st.plotly_chart(fig_2)
 
 
 @st.cache(persist=True)
@@ -115,16 +115,16 @@ if len(choice) > 0:
 
 st.sidebar.header("Word Cloud")
 word_sentiment = st.sidebar.radio('Display word cloud for what sentiment?', ('positive', 'neutral', 'negative'))
-if not st.sidebar.checkbox("Close", True, key='3'):
-    st.subheader('Word cloud for %s sentiment' % (word_sentiment))
-    df = data[data['airline_sentiment']==word_sentiment]
-    words = ' '.join(df['text'])
-    processed_words = ' '.join([word for word in words.split() if 'http' not in word and not word.startswith('@') and word != 'RT'])
-    wordcloud = WordCloud(stopwords=STOPWORDS, background_color='white', width=800, height=640).generate(processed_words)
-    #deprecation.showPyplotGlobalUs
-    st.set_option('deprecation.showPyplotGlobalUse', False)
-    plt.imshow(wordcloud)
-    plt.xticks([])
-    plt.yticks([])
-    st.pyplot()
+#if not st.sidebar.checkbox("Close", True, key='3'):
+st.subheader('Word cloud for %s sentiment' % (word_sentiment))
+df = data[data['airline_sentiment']==word_sentiment]
+words = ' '.join(df['text'])
+processed_words = ' '.join([word for word in words.split() if 'http' not in word and not word.startswith('@') and word != 'RT'])
+wordcloud = WordCloud(stopwords=STOPWORDS, background_color='white', width=800, height=640).generate(processed_words)
+#deprecation.showPyplotGlobalUs
+st.set_option('deprecation.showPyplotGlobalUse', False)
+plt.imshow(wordcloud)
+plt.xticks([])
+plt.yticks([])
+st.pyplot()
     
